41cdbc61445561202d5437fe8e408add55cc9c28
Adrian Słowiński <adrian.slowinski87@gmail.com>
2019-04-04 13:45:31 +0200

    Add form

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
 
 class AddTask extends Component {
-  state = {  }
+  state = { 
+    text: '',
+    checked: false,
+    date: '2019-04-04'
+   }
   render() { 
     return ( 
-      <div>Dodaj task</div>
+      <div>
+        <input 
+          type="text" 
+          placholder="dodaj zadanie" 
+          value={this.state.text}/>
+        <input 
+          type="checkbox" 
+          checked={this.state.checked} 
+          id="important"/>
+        <label htmlFor="important">Priorytet</label>
+        <br/>
+        <label htmlFor="date">Do kiedy zrobić</label>
+        <br />
+        <input type="date" value={this.state.date}  min="2019-04-04" max="2021-04-04"/>
+        <button>Dodaj</button>
+      </div>
      );
   }
 }
